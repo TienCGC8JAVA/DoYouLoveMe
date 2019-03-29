288af454d69dee4130c009ac2572c521f63a3606
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-03-29 23:07:18 +0700

    Ứng dụng đơn giản

---
+++ DoYouLoveMe.js
@@ -0,0 +1,12 @@
+function yes()
+{
+    alert("Me too!");
+}
+
+function onMouseOver()
+{
+    document.getElementById("btnNo").style.left = Math.random()*window.innerWidth + 'px';
+    document.getElementById("btnNo").style.top = Math.random()*window.innerHeight + 'px';
+}
+
+
